5d3fd7ad30edcabb0509101193634856ea8876dd
BEnh3N <bhpohlmann@icloud.com>
2023-12-23 20:21:38 -0800

    added alpha dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    -- alpha dashboard
+    {
+        'goolord/alpha-nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.startify'.config)
+        end
+    };
+}
